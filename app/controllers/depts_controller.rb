be6ba6cecfe19a30c874bec55f1f59b03f125765
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-02-20 17:02:43 -0700

    beta dept store

---
+++ app/controllers/depts_controller.rb
@@ -0,0 +1,47 @@
+class DeptsController < ApplicationController
+  before_action :set_dept, only: [:show, :edit, :update, :destroy]
+  def index
+    @depts = Dept.all 
+  end
+
+  def show
+  end
+
+  def new
+    @dept = Dept.new
+  end
+
+  def edit
+  end
+
+  def create
+    @dept = Dept.new(dept_params)
+    if @dept.save
+      redirect_to depts_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @dept.update(dept_params)
+      redirect_to @dept
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dept.destroy
+    redirect_to depts_path
+  end
+
+  private
+    def set_dept
+      @dept = Dept.find(params[:id])
+    end
+
+    def dept_params
+      params.require(:dept).permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root "depts#index"
+  resources :depts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
